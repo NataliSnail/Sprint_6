83ffc94016fe8e47509e3801669996d39e312bf0
Наталья Никитина <153123991+NataliSnail@users.noreply.github.com>
2024-01-24 11:56:59 +0300

    Update test_order_page.py

--- test/test_order_page.py
+++ test/test_order_page.py
@@ -7,6 +7,7 @@ import allure
 
 class TestOrderPage:
     @allure.title("Заполнение форма 'сделать заказ'")
+    @allure.description('Нажать на кнопку "Заказать" > заполнить форму заказа > перейти к форме аренды')
     @pytest.mark.parametrize("name,last_name,address, metro_station,tel_number",
                              [
                                 ('Иван', 'Иванов', 'Москва, ул. Филевская, д1, кв.1', OrderFormPageLocators.METRO_STATION_SOKOLNIKI, '49512365478'),
@@ -20,6 +21,7 @@ class TestOrderPage:
         order_form.get_rent_form()
 
     @allure.title("Проверить переход по логотипу 'Самокат' со страницы 'Оформления заказа'")
+    @allure.description('Нажать на кнопку "Заказать" > нажать на логотип Самокат > перейти на главную страницу сайта')
     def test_click_logo_samokat(self,driver):
         logo_samokat = OrderPage(driver,Urls.URL)
         logo_samokat.click_button_order_up()
