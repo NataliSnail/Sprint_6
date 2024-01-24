74e1c8231cacae573c7ca5b4fbf08cf56f6fa7a6
Наталья Никитина <153123991+NataliSnail@users.noreply.github.com>
2024-01-24 11:52:02 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,14 +1,18 @@
-from constants import Urls
 from selenium import webdriver
+from selenium.webdriver.firefox.service import Service
+from webdriver_manager.firefox import GeckoDriverManager
+from constants import Urls
 import pytest
 from locators.order_page_locators import OrderFormPageLocators
 from locators.main_page_locators import MainPageLocators
 from constants import Constants
 
 
+
 @pytest.fixture
 def driver():
-    driver = webdriver.Firefox()
+    service = Service(GeckoDriverManager().install())
+    driver = webdriver.Firefox(service=service)
     driver.get(Urls.URL)
     yield driver
     driver.quit()
