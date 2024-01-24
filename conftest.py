52958931afed10850925c4f7ab763c92c7f10604
Наталья Никитина <153123991+NataliSnail@users.noreply.github.com>
2024-01-24 23:05:46 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -18,17 +18,7 @@ def driver():
     driver.quit()
 
 
-@pytest.fixture
-def fill_order_form(driver):
-    driver.find_element(*MainPageLocators.BUTTON_ORDER_UP).click()
-    driver.find_element(*OrderFormPageLocators.NAME_FIELD).send_keys(Constants.name)
-    driver.find_element(*OrderFormPageLocators.LAST_NAME_FIELD).send_keys(Constants.last_name)
-    driver.find_element(*OrderFormPageLocators.ADDRESS_FIELD).send_keys(Constants.address_field)
-    driver.find_element(*OrderFormPageLocators.STATION_METRO_FIELD).click()
-    driver.find_element(*OrderFormPageLocators.METRO_STATION_SOKOLNIKI).click()
-    driver.find_element(*OrderFormPageLocators.PHONE_NUMBER_FIELD).send_keys(Constants.phone_number)
-    driver.find_element(*OrderFormPageLocators.NEXT_BUTTON).click()
-    return driver
+
 
 
 
